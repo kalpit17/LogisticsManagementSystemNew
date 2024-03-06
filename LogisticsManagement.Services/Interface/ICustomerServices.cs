f6a9c6003ce88a22f15fb9d2fbd337e4fb3d50d1
kalpit <kalpitprajapati17@gmail.com>
2024-03-07 03:16:43 +0530

    Add project files.

---
+++ LogisticsManagement.Services/Interface/ICustomerServices.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogisticsManagement.Services.Interface
+{
+    public interface ICustomerServices
+    {
+        void ViewAllProducts();
+        void ViewProductDetails(int productId);
+
+        bool ValidateQuantity(int productId, int quantity);
+
+        bool BuyProduct(int userId, int productId, int quantity);
+
+        void ViewAllOrders(int userId);
+
+        void ViewOrderDetails(int userId, int orderId);
+
+    }
+}
